d7c8815eca0f7f2c7b98c27c16bf29343f4aa8b3
DvorakuvPadawan <hajnydaniel@gmail.com>
2023-09-16 17:32:06 +0200

    refactored

--- scripts/test_fork.py
+++ scripts/test_fork.py
@@ -1,5 +1,4 @@
 from brownie import interface, accounts
-from web3 import Web3
 
 LINK_ADDRESS = "0x514910771AF9Ca656af840dff83E8264EcF986CA"
 
@@ -7,13 +6,8 @@ def get_contract_link_balance(holder_address):
     link_contract = interface.Link(LINK_ADDRESS)
     print(link_contract.balanceOf(holder_address))
     holder = accounts.at(holder_address, force = True)
-    #holder = accounts(holder_address)
-    #holder = interface.Contract(holder_address)
     balance = holder.balance()/ 10**18
     print(balance)
-    #print(web3.eth.getBalance(holder_address))
-    #balance = accounts.at(holder_address, force=True).balance()
-    #print(f"The balance of {holder_address} is {balance} ETH")
     account = accounts[0]
     amount = 10 * 10 ** 18
     account.transfer(holder_address, amount)
